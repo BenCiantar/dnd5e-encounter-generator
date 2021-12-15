683697ebbfbe3fe3a232221f0998a1c2b63d3bb6
Ben Ciantar <benjamin.ciantar@hyperisland.se>
2021-12-15 16:54:33 +0100

    Added XP values to monsters

--- scripts/script.js
+++ scripts/script.js
@@ -323,106 +323,106 @@ function calculateXP(CR){
 
   switch (CR){
     case 0:
-      XP = 0;
+      XP = 10;
       return XP;
     case "eighth":
-      XP = 0;
+      XP = 25;
       return XP;
     case "quarter":
-      XP = 0;
+      XP = 50;
       return XP;
     case "half":
-      XP = 0;
+      XP = 100;
       return XP;
     case 1:
-      XP = 0;
+      XP = 200;
       return XP;
     case 2:
-      XP = 0;
+      XP = 450;
       return XP;
     case 3:
-      XP = 0;
+      XP = 700;
       return XP;
     case 4:
-      XP = 0;
+      XP = 1100;
       return XP;
     case 5:
-      XP = 0;
+      XP = 1800;
       return XP;
     case 6:
-      XP = 0;
+      XP = 2300;
       return XP;
     case 7:
-      XP = 0;
+      XP = 2900;
       return XP;
     case 8:
-      XP = 0;
+      XP = 3900;
       return XP;
     case 9:
-      XP = 0;
+      XP = 5000;
       return XP;
     case 10:
-      XP = 0;
+      XP = 5900;
       return XP;
     case 11:
-      XP = 0;
+      XP = 7200;
       return XP;
     case 12:
-      XP = 0;
+      XP = 8400;
       return XP;
     case 13:
-      XP = 0;
+      XP = 10000;
       return XP;
     case 14:
-      XP = 0;
+      XP = 11500;
       return XP;
     case 15:
-      XP = 0;
+      XP = 13000;
       return XP;
     case 16:
-      XP = 0;
+      XP = 15000;
       return XP;
     case 17:
-      XP = 0;
+      XP = 18000;
       return XP;
     case 18:
-      XP = 0;
+      XP = 20000;
       return XP;
     case 19:
-      XP = 0;
+      XP = 22000;
       return XP;
     case 20:
-      XP = 0;
+      XP = 25000;
       return XP;
     case 21:
-      XP = 0;
+      XP = 33000;
       return XP;
     case 22:
-      XP = 0;
+      XP = 41000;
       return XP;
     case 23:
-      XP = 0;
+      XP = 50000;
       return XP;
     case 24:
-      XP = 0;
+      XP = 62000;
       return XP;
     case 25:
-      XP = 0;
+      XP = 75000;
       return XP;
     case 26:
-      XP = 0;
+      XP = 90000;
       return XP;
     case 27:
-      XP = 0;
+      XP = 105000;
       return XP;
     case 28:
-      XP = 0;
+      XP = 120000;
       return XP;
     case 29:
-      XP = 0;
+      XP = 135000;
       return XP;
     case 30:
-      XP = 0;
+      XP = 155000;
       return XP;
   }
 }
