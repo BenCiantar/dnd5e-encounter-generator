d53000f4ad14b7581797e08cd1e87b7dc20ecba6
Ben Ciantar <benjamin.ciantar@hyperisland.se>
2021-12-16 14:46:41 +0100

    Fixed NaN Xp bug for fraction CRs

--- scripts/script.js
+++ scripts/script.js
@@ -251,10 +251,11 @@ function addToEncounter(name) {
   for (let i = 0; i < monsterArray.results.length; i++) {
     if (name == monsterArray.results[i].name) {
       if (encounterArray.length == 0) {
+        monsterArray.results[i].challenge_rating
         encounterArray.push(
           {
             name: monsterArray.results[i].name,
-            xp: convertCRToXP(eval(monsterArray.results[i].challenge_rating)),
+            xp: convertCRToXP(monsterArray.results[i].challenge_rating),
             count: 1
           }
         )
@@ -274,7 +275,7 @@ function addToEncounter(name) {
         encounterArray.push(
           {
             name: monsterArray.results[i].name,
-            xp: convertCRToXP(eval(monsterArray.results[i].challenge_rating)),
+            xp: convertCRToXP(monsterArray.results[i].challenge_rating),
             count: 1
           }
         )
@@ -287,9 +288,8 @@ function addToEncounter(name) {
 function updateEncounterList(){
   document.getElementById("encounter-top").innerHTML = "";
 
-  // console.log(typeof encounterArray[i].count, typeof)
-
   for (let i = 0; i < encounterArray.length; i++){
+    console.log(encounterArray[i].xp);
     document.getElementById("encounter-top").innerHTML += `
       <div class="encounter-list-item">
         <div class="encounter-list-left">
@@ -299,7 +299,7 @@ function updateEncounterList(){
         x ${encounterArray[i].count}
         </div>
         <div class="encounter-list-right">
-        ${encounterArray[i].xp * encounterArray[i].count}xp
+        ${parseInt(encounterArray[i].xp) * encounterArray[i].count}xp
         </div>
       </div>
     `
@@ -324,8 +324,6 @@ function populateMonsterList(data) {
       }
 
       try {
-        // let monsterName = data.results[i].name.replace(/\s+/g, '');
-
         document.getElementById(`cr-${CR}`).innerHTML += `
         <div class="monster-item">
           <div class="monster-summary">
@@ -420,106 +418,106 @@ function convertCRToXP(CR){
   let XP;
 
   switch (CR){
-    case 0:
+    case "0":
       XP = 10;
       return XP;
-    case "eighth":
+    case "1/8":
       XP = 25;
       return XP;
-    case "quarter":
+    case "1/4":
       XP = 50;
       return XP;
-    case "half":
+    case "1/2":
       XP = 100;
       return XP;
-    case 1:
+    case "1":
       XP = 200;
       return XP;
-    case 2:
+    case "2":
       XP = 450;
       return XP;
-    case 3:
+    case "3":
       XP = 700;
       return XP;
-    case 4:
+    case "4":
       XP = 1100;
       return XP;
-    case 5:
+    case "5":
       XP = 1800;
       return XP;
-    case 6:
+    case "6":
       XP = 2300;
       return XP;
-    case 7:
+    case "7":
       XP = 2900;
       return XP;
-    case 8:
+    case "8":
       XP = 3900;
       return XP;
-    case 9:
+    case "9":
       XP = 5000;
       return XP;
-    case 10:
+    case "10":
       XP = 5900;
       return XP;
-    case 11:
+    case "11":
       XP = 7200;
       return XP;
-    case 12:
+    case "12":
       XP = 8400;
       return XP;
-    case 13:
+    case "13":
       XP = 10000;
       return XP;
-    case 14:
+    case "14":
       XP = 11500;
       return XP;
-    case 15:
+    case "15":
       XP = 13000;
       return XP;
-    case 16:
+    case "16":
       XP = 15000;
       return XP;
-    case 17:
+    case "17":
       XP = 18000;
       return XP;
-    case 18:
+    case "18":
       XP = 20000;
       return XP;
-    case 19:
+    case "19":
       XP = 22000;
       return XP;
-    case 20:
+    case "20":
       XP = 25000;
       return XP;
-    case 21:
+    case "21":
       XP = 33000;
       return XP;
-    case 22:
+    case "22":
       XP = 41000;
       return XP;
-    case 23:
+    case "23":
       XP = 50000;
       return XP;
-    case 24:
+    case "24":
       XP = 62000;
       return XP;
-    case 25:
+    case "25":
       XP = 75000;
       return XP;
-    case 26:
+    case "26":
       XP = 90000;
       return XP;
-    case 27:
+    case "27":
       XP = 105000;
       return XP;
-    case 28:
+    case "28":
       XP = 120000;
       return XP;
-    case 29:
+    case "29":
       XP = 135000;
       return XP;
-    case 30:
+    case "30":
       XP = 155000;
       return XP;
   }
