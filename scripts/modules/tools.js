aa4ebf93eb8558b2bc3d1a52b0ba57ed8e67bb91
Ben Ciantar <benjamin.ciantar@hyperisland.se>
2021-12-19 22:17:16 +0100

    Refactored code to remove all in-html javascript calls to allow for switch to modules

---
+++ scripts/modules/tools.js
@@ -0,0 +1,116 @@
+
+
+
+export function convertCrToXp(CR){
+    let XP;
+  
+    switch (CR){
+      case "0":
+        XP = 10;
+        return XP;
+      case "1/8":
+        XP = 25;
+        return XP;
+      case "1/4":
+        XP = 50;
+        return XP;
+      case "1/2":
+        XP = 100;
+        return XP;
+      case "1":
+        XP = 200;
+        return XP;
+      case "2":
+        XP = 450;
+        return XP;
+      case "3":
+        XP = 700;
+        return XP;
+      case "4":
+        XP = 1100;
+        return XP;
+      case "5":
+        XP = 1800;
+        return XP;
+      case "6":
+        XP = 2300;
+        return XP;
+      case "7":
+        XP = 2900;
+        return XP;
+      case "8":
+        XP = 3900;
+        return XP;
+      case "9":
+        XP = 5000;
+        return XP;
+      case "10":
+        XP = 5900;
+        return XP;
+      case "11":
+        XP = 7200;
+        return XP;
+      case "12":
+        XP = 8400;
+        return XP;
+      case "13":
+        XP = 10000;
+        return XP;
+      case "14":
+        XP = 11500;
+        return XP;
+      case "15":
+        XP = 13000;
+        return XP;
+      case "16":
+        XP = 15000;
+        return XP;
+      case "17":
+        XP = 18000;
+        return XP;
+      case "18":
+        XP = 20000;
+        return XP;
+      case "19":
+        XP = 22000;
+        return XP;
+      case "20":
+        XP = 25000;
+        return XP;
+      case "21":
+        XP = 33000;
+        return XP;
+      case "22":
+        XP = 41000;
+        return XP;
+      case "23":
+        XP = 50000;
+        return XP;
+      case "24":
+        XP = 62000;
+        return XP;
+      case "25":
+        XP = 75000;
+        return XP;
+      case "26":
+        XP = 90000;
+        return XP;
+      case "27":
+        XP = 105000;
+        return XP;
+      case "28":
+        XP = 120000;
+        return XP;
+      case "29":
+        XP = 135000;
+        return XP;
+      case "30":
+        XP = 155000;
+        return XP;
+    }
+  }
+
+
+
+
+
--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+import { convertCrToXp } from './modules/tools.js';
+
 //////////////////////////////Globals
 
 const apiLink = "https://api.open5e.com/monsters/?limit=2000";
@@ -27,6 +29,9 @@ document.addEventListener("DOMContentLoaded", function() {
   updatePlayerInfo();
 });
 
+addListener("change", "number-of-players", updatePlayerInfo);
+addListener("change", "player-one", updateXpThresholds);
+
 function fetchApi() {
     fetch(apiLink, {
       })
@@ -41,7 +46,7 @@ function fetchApi() {
 
 function updatePlayerInfo() {
   refreshPlayerList();
-  updateXPThresholds();
+  updateXpThresholds();
 }
 
 
@@ -52,14 +57,24 @@ function refreshPlayerList() {
 
   let numPlayersInt = document.getElementById('number-of-players').value;
 
-  for (i = 0; i < numPlayersInt; i++) {
+  createLevelSelectors(numPlayersInt);
+
+  for (let i = 0; i < numPlayersInt; i++) {
+    let playerNumString = convertNumPlayersToString(i);
+
+    addListener("change", `player-${playerNumString}`, updateXpThresholds);
+  }
+}
+
+function createLevelSelectors(numPlayersInt){
+  for (let i = 0; i < numPlayersInt; i++) {
 
     let playerNumString = convertNumPlayersToString(i);
 
     document.getElementById("player-display").innerHTML += `
     <div class="player-level-selector">
       <label for="player-${playerNumString}">Player Level:</label>
-      <select name="players" class="player-lvl" id="player-${playerNumString}" onchange="updateXPThresholds()">
+      <select name="players" class="player-lvl" id="player-${playerNumString}">
           <option value="1">1</option>
           <option value="2">2</option>
           <option value="3">3</option>
@@ -82,12 +97,11 @@ function refreshPlayerList() {
           <option value="20">20</option>
       </select>
     </div>
-  `
+    `
   }
 }
 
-function updateXPThresholds() {
-
+function updateXpThresholds() {
   xpThresholds.easyXpThreshold = 0;
   xpThresholds.mediumXpThreshold = 0;
   xpThresholds.hardXpThreshold = 0;
@@ -235,7 +249,7 @@ function updateXPThresholds() {
 function addToEncounter(name) {
   for (let i = 0; i < monsterArray.results.length; i++) {
     if (name == monsterArray.results[i].name) {
-      let CR = convertCRToXP(monsterArray.results[i].challenge_rating);
+      let CR = convertCrToXp(monsterArray.results[i].challenge_rating);
       keyStats.xpTotal += CR;
       keyStats.monsterCount++;
       if (encounterArray.length == 0) {
@@ -289,7 +303,7 @@ function updateEncounterList(){
   for (let i = 0; i < encounterArray.length; i++){
     document.getElementById("encounter-top").innerHTML += `
       <div class="encounter-list-item">
-        <div class="encounter-list-close" onclick="removeFromEncounter(${i})">
+        <div id="close-${i}" class="encounter-list-close">
         &#10005;
         </div>
         <div class="encounter-list-left">
@@ -303,6 +317,13 @@ function updateEncounterList(){
         </div>
       </div>
     `
+
+    document.getElementById(`close-${i}`).addEventListener("click", function() {
+      removeFromEncounter();
+    });
+  }
+  for (let i = 0; i < encounterArray.length; i++){
+    addListener("click", `close-${i}`, removeFromEncounter, `${i}`);
   }
   updateMonsterSummary();
 }
@@ -386,32 +407,35 @@ function populateMonsterList(data) {
 
   for (let i = 0; i < data.results.length; i++){
 
-      let CR = data.results[i].challenge_rating;
-      let CRid = CR;
+    let CR = data.results[i].challenge_rating;
+    let CRid = CR;
 
-      //CODE CHECK move fraction converter into a separate function
-      if (CR == "1/8") {
-        CRid = "eighth";
-      } else if (CR == "1/4") {
-        CRid = "quarter";
-      } else if (CR == "1/2") {
-        CRid = "half";
-      }
+    //CODE CHECK move fraction converter into a separate function
+    if (CR == "1/8") {
+      CRid = "eighth";
+    } else if (CR == "1/4") {
+      CRid = "quarter";
+    } else if (CR == "1/2") {
+      CRid = "half";
+    }
 
-      try {
-        document.getElementById(`cr-${CRid}`).innerHTML += `
-        <div class="monster-item">
-          <div class="monster-summary">
-            <h4>${data.results[i].name}</h4><p>CR: ${CR} - XP: ${convertCRToXP(CR)}</p>
-          </div>
-          <div class="add-monster-section">
-            <button onclick="addToEncounter('${data.results[i].name}')">Add</button>
-          </div>
+    try {
+      document.getElementById(`cr-${CRid}`).innerHTML += `
+      <div class="monster-item">
+        <div class="monster-summary">
+          <h4>${data.results[i].name}</h4><p>CR: ${CR} - XP: ${convertCrToXp(CR)}</p>
         </div>
-      `
-      } catch (error) {
-        console.log("Error with item: " + data.results[i].name);
-      }
+        <div class="add-monster-section">
+          <button id="${data.results[i].name}-btn">Add</button>
+        </div>
+      </div>
+    `
+    } catch (error) {
+      console.log("Error with item: " + data.results[i].name);
+    }
+  }
+  for (let i = 0; i < data.results.length; i++){
+    addListener("click", `${data.results[i].name}-btn`, addToEncounter, `${data.results[i].name}`);
   }
 }
 
@@ -506,113 +530,10 @@ function calculateMultiplier(count) {
 }
 
 //CODE CHECK move to a utilities javascript module
-function convertCRToXP(CR){
-  let XP;
-
-  switch (CR){
-    case "0":
-      XP = 10;
-      return XP;
-    case "1/8":
-      XP = 25;
-      return XP;
-    case "1/4":
-      XP = 50;
-      return XP;
-    case "1/2":
-      XP = 100;
-      return XP;
-    case "1":
-      XP = 200;
-      return XP;
-    case "2":
-      XP = 450;
-      return XP;
-    case "3":
-      XP = 700;
-      return XP;
-    case "4":
-      XP = 1100;
-      return XP;
-    case "5":
-      XP = 1800;
-      return XP;
-    case "6":
-      XP = 2300;
-      return XP;
-    case "7":
-      XP = 2900;
-      return XP;
-    case "8":
-      XP = 3900;
-      return XP;
-    case "9":
-      XP = 5000;
-      return XP;
-    case "10":
-      XP = 5900;
-      return XP;
-    case "11":
-      XP = 7200;
-      return XP;
-    case "12":
-      XP = 8400;
-      return XP;
-    case "13":
-      XP = 10000;
-      return XP;
-    case "14":
-      XP = 11500;
-      return XP;
-    case "15":
-      XP = 13000;
-      return XP;
-    case "16":
-      XP = 15000;
-      return XP;
-    case "17":
-      XP = 18000;
-      return XP;
-    case "18":
-      XP = 20000;
-      return XP;
-    case "19":
-      XP = 22000;
-      return XP;
-    case "20":
-      XP = 25000;
-      return XP;
-    case "21":
-      XP = 33000;
-      return XP;
-    case "22":
-      XP = 41000;
-      return XP;
-    case "23":
-      XP = 50000;
-      return XP;
-    case "24":
-      XP = 62000;
-      return XP;
-    case "25":
-      XP = 75000;
-      return XP;
-    case "26":
-      XP = 90000;
-      return XP;
-    case "27":
-      XP = 105000;
-      return XP;
-    case "28":
-      XP = 120000;
-      return XP;
-    case "29":
-      XP = 135000;
-      return XP;
-    case "30":
-      XP = 155000;
-      return XP;
-  }
+function addListener(type, id, callback, param){
+  document.getElementById(id).addEventListener(type, function (){
+    callback(param);
+  });
 }
 
 
