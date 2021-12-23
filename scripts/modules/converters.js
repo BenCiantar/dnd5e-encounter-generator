bdadf4ef344ee88ccd73e48c0eb19deaffac6743
Ben Ciantar <benjamin.ciantar@hyperisland.se>
2021-12-23 14:53:08 +0100

    Simplified convertCrToXp function

--- scripts/modules/converters.js
+++ scripts/modules/converters.js
@@ -1,109 +1,73 @@
-export function convertCrToXp(CR){
-    let XP;
-  
-    switch (CR){
+export function convertCrToXp(cr){
+    switch (cr){
       case "0":
-        XP = 10;
-        return XP;
+        return 10;
       case "1/8":
-        XP = 25;
-        return XP;
+        return 25;
       case "1/4":
-        XP = 50;
-        return XP;
+        return 50;
       case "1/2":
-        XP = 100;
-        return XP;
+        return 100;
       case "1":
-        XP = 200;
-        return XP;
+        return 200;
       case "2":
-        XP = 450;
-        return XP;
+        return 450;
       case "3":
-        XP = 700;
-        return XP;
+        return 700;
       case "4":
-        XP = 1100;
-        return XP;
+        return 1100;
       case "5":
-        XP = 1800;
-        return XP;
+        return 1800;
       case "6":
-        XP = 2300;
-        return XP;
+        return 2300;
       case "7":
-        XP = 2900;
-        return XP;
+        return 2900;
       case "8":
-        XP = 3900;
-        return XP;
+        return 3900;
       case "9":
-        XP = 5000;
-        return XP;
+        return 5000;
       case "10":
-        XP = 5900;
-        return XP;
+        return 5900;
       case "11":
-        XP = 7200;
-        return XP;
+        return 7200;
       case "12":
-        XP = 8400;
-        return XP;
+        return 8400;
       case "13":
-        XP = 10000;
-        return XP;
+        return 10000;
       case "14":
-        XP = 11500;
-        return XP;
+        return 11500;
       case "15":
-        XP = 13000;
-        return XP;
+        return 13000;
       case "16":
-        XP = 15000;
-        return XP;
+        return 15000;
       case "17":
-        XP = 18000;
-        return XP;
+        return 18000;
       case "18":
-        XP = 20000;
-        return XP;
+        return 20000;
       case "19":
-        XP = 22000;
-        return XP;
+        return 22000;
       case "20":
-        XP = 25000;
-        return XP;
+        return 25000;
       case "21":
-        XP = 33000;
-        return XP;
+        return 33000;
       case "22":
-        XP = 41000;
-        return XP;
+        return 41000;
       case "23":
-        XP = 50000;
-        return XP;
+        return 50000;
       case "24":
-        XP = 62000;
-        return XP;
+        return 62000;
       case "25":
-        XP = 75000;
-        return XP;
+        return 75000;
       case "26":
-        XP = 90000;
-        return XP;
+        return 90000;
       case "27":
-        XP = 105000;
-        return XP;
+        return 105000;
       case "28":
-        XP = 120000;
-        return XP;
+        return 120000;
       case "29":
-        XP = 135000;
-        return XP;
+        return 135000;
       case "30":
-        XP = 155000;
-        return XP;
+        return 155000;
     }
   }
   
